9513ef23ffe2024b40437ad2b5de28bfcb06e275
Michelle <mliuu3259@gmail.com>
2023-03-14 16:04:41 -0400

    ???

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,12 +1,13 @@
-export default function QueryProcessor(query: string): number {
-  // if (query.toLowerCase().includes('plus')) {
-  //   return (
-  //     'William Shakespeare (26 April 1564 - 23 April 1616) was an ' +
-  //     'English poet, playwright, and actor, widely regarded as the greatest ' +
-  //     "writer in the English language and the world's pre-eminent dramatist."
-  //   );
-  // }
+export default function QueryProcessor(query: string): string {
+  if (query.toLowerCase().includes('is the largest')) {
+    const nums = query
+      .replaceAll('Which of the following numbers is the largest: ', '')
+      .replaceAll('?', '')
+      .split(', ')
+      .map((w) => parseInt(w));
+    return String(Math.max(...nums));
+  }
 
   console.log(query);
-  return 89;
+  // return 89;
 }
