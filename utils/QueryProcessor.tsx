f4cfece1f9e7d2e68d7c362d32d4219ad773ee1e
Michelle <mliuu3259@gmail.com>
2023-03-14 15:56:22 -0400

    ???

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,11 +1,3 @@
-export default function QueryProcessor(query: string): string {
-  if (query.toLowerCase().includes('shakespeare')) {
-    return (
-      'William Shakespeare (26 April 1564 - 23 April 1616) was an ' +
-      'English poet, playwright, and actor, widely regarded as the greatest ' +
-      "writer in the English language and the world's pre-eminent dramatist."
-    );
-  }
-
-  return 'michelle & richelle';
+export default function QueryProcessor(query: string): number {
+  return 89;
 }
