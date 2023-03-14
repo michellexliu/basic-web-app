2f36dc76362804eadb1a3ccb76d41c8040e32d39
Michelle <mliuu3259@gmail.com>
2023-03-14 15:57:30 -0400

    ???

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,3 +1,12 @@
 export default function QueryProcessor(query: string): number {
+  // if (query.toLowerCase().includes('plus')) {
+  //   return (
+  //     'William Shakespeare (26 April 1564 - 23 April 1616) was an ' +
+  //     'English poet, playwright, and actor, widely regarded as the greatest ' +
+  //     "writer in the English language and the world's pre-eminent dramatist."
+  //   );
+  // }
+
+  console.log(query);
   return 89;
 }
