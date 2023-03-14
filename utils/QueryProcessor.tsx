94ca188e98201631d7e16cb52eee6194247add00
Michelle <mliuu3259@gmail.com>
2023-03-14 16:13:17 -0400

    //

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,4 +1,22 @@
+function isSquareAndCube(num: number) {
+  // Check if the number is a perfect square
+  const squareRoot = Math.sqrt(num);
+  if (squareRoot !== Math.floor(squareRoot)) {
+    return false;
+  }
+
+  // Check if the number is a perfect cube
+  const cubeRoot = Math.cbrt(num);
+  if (cubeRoot !== Math.floor(cubeRoot)) {
+    return false;
+  }
+
+  // The number is both a square and a cube
+  return true;
+}
+
 export default function QueryProcessor(query: string): string {
+  const lower = query.toLowerCase().replace('?', '');
   if (query.toLowerCase().includes('is the largest')) {
     const nums = query
       .replaceAll('Which of the following numbers is the largest: ', '')
@@ -13,8 +31,20 @@ export default function QueryProcessor(query: string): string {
       .split(' plus ')
       .map((w) => parseInt(w));
     return String(nums[0] + nums[1]);
+  } else if (lower.includes('square and a cube')) {
+    const nums = query
+      .replaceAll(
+        'Which of the following numbers is both a square and a cube: ',
+        ''
+      )
+      .replaceAll('?', '')
+      .split(', ')
+      .map((w) => parseInt(w));
+
+    const filtered = nums.filter((num) => isSquareAndCube(num));
+
+    return String(filtered[0]);
   }
 
-  console.log(query);
   return '89';
 }
